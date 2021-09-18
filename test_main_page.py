a3247327a4cac121547691c9964ef7b0afa8992c
Narcologer <velt-nikita@yandex.ru>
2021-09-18 12:59:59 +0300

    Give more PageObject logic, added is_element_present to base_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,13 @@
+from .pages.main_page import MainPage
+
 link = "http://selenium1py.pythonanywhere.com/"
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
-
 def test_guest_can_go_to_login_page(browser):
-   browser.get(link) 
-   go_to_login_page(browser) 
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
